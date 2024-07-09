8b06c88c4abb703a8b880dcb1b1fc8b9ce857482
chinl2003 <nguyenlchi220602@gmail.com>
2024-07-10 00:16:04 +0700

    Add initial project

---
+++ App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using ManageStored;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using System.Windows;
+using Repositories.Repository.User;
+using Services.Services;
+using Microsoft.EntityFrameworkCore;
+using Repositories.EF;
+
+namespace StoreManagement
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    /// 
+    public partial class App : Application
+    {
+        private IHost _host;
+
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddDbContext<MyStoreDBContext>(options =>
+                        options.UseSqlServer(context.Configuration.GetConnectionString("MyStore")));
+
+                    services.AddScoped<IUserRepository, UserRepository>();
+                    services.AddScoped<IUserService, UserService>();
+                    services.AddScoped<Login>();
+                })
+                .Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await _host.StartAsync();
+            var loginWindow = _host.Services.GetRequiredService<Login>();
+            loginWindow.Show();
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await _host.StopAsync();
+            _host.Dispose();
+            base.OnExit(e);
+        }
+    }
+
+}
---
+++ Login.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Repositories.Repository.User;
+using Services.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Services;
+using Repositories; 
+
+namespace ManageStored
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        private readonly IUserService _userService;
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+        public Login(IUserService userService)
+        {
+            InitializeComponent();
+            _userService = userService;
+        }
+        private void Window_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private async void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUserName.Text;
+            string password = txtPassword.Password;
+
+            if (await _userService.LoginAsync(username, password))
+            {
+                MessageBox.Show("Login successful!");
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password.");
+            }
+        }
+
+    }
+}
